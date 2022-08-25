b1a703ecbc5a887cda52dc036152b7b2d82b9c22
Aditya Jyoti <adityajyoti2002@gmail.com>
2022-08-25 11:08:07 +0530

    change

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cin>>marks;
+    if(marks > 80){
+        cout<<"lets party"<<endl;
+    }
+    return 0;
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        int a = 2;
+        if(a+a == 4){
+            System.out.println("equal hai!");
+        }
+    }
+}
